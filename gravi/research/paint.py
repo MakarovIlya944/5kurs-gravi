85a52a6ac3d459a4162bb3535e92e959f5af5012
Ilya Makarov <makarovilya944@yandex.ru>
2020-11-13 14:58:26 +0700

    Add config show

--- gravi/research/paint.py
+++ gravi/research/paint.py
@@ -3,27 +3,39 @@ import numpy.linalg as l
 from .data import DataReader
 
 
-def show_predict(predicted_data, model_index=None, dataset_index=None):
+def show_predict(predicted_data, model_index=None, dataset_index=None, paint_show=None):
   data = predicted_data['data']
-  x = [i for i in range(len(data))]
-  y = [d['l2_diff'] for d in data]
-  labels = [s['name'] for s in data]
-  if model_index and dataset_index:
-    x,y,c = DataReader.read_folder('data/' + data[model_index]['dataset'])
-    true_y = y[dataset_index]
-    pred_y = data[model_index]['predicted'][dataset_index]
-    c = c[dataset_index]
-    dx, dy, dz, sm = 10, 10, 10, 1
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    m = max(true_y)
-    for j, v in enumerate(true_y):
-      ax.scatter(j % c[0] * dx, j % (c[0] * c[1]) // c[1] * dy, j // (c[0] * c[1])  * -dz, marker='o', c=[[1,0,0,abs(pred_y[j] - v)/m]])
-      ax.scatter(j % c[0] * dx + sm, j % (c[0] * c[1]) // c[1] * dy + sm, j // (c[0] * c[1])  * -dz + sm, marker='o', c=[[0,0,1,v/m]])
+  if model_index and dataset_index and paint_show:
+    try:
+      x,y,c = DataReader.read_folder('data/' + data[model_index]['dataset'])
+      true_y = y[dataset_index]
+      pred_y = data[model_index]['predicted'][dataset_index]
+      c = c[dataset_index]
+      vm = max(true_y)
 
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+      n_plotsx, n_plotsy = 3, 4
+      fig, axs = plt.subplots(n_plotsx, n_plotsy)
+      for i in range(c[2]):
+        true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+        pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+        cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
+
+        j = i // 4
+        k = i % 4
+        m = [cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+        if paint_show == 'true':
+          m = [true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+        elif paint_show == 'pred':
+          m = [pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+        axs[j, k].matshow(m,vmax=vm,vmin=0,cmap="Reds")
+        axs[j, k].set_title(f'z: {-i}')
+      plt.tight_layout()
+    except IndexError as e:
+      print(str(e))
+      raise e
   else:
+    x = [i for i in range(len(data))]
+    y = [d['l2_diff'] for d in data]
+    labels = [s['name'] for s in data]
     plt.bar(x,y,tick_label=labels)
   plt.show()
--- main.py
+++ main.py
@@ -31,18 +31,23 @@ def main():
     elif sys.argv[1] == 'predict':
       model_index = None
       dataset_index = None
+      show_type = None
       if len(sys.argv) > 4:
         model_index = int(sys.argv[3])
         dataset_index = int(sys.argv[4])
+        if len(sys.argv) > 5:
+          show_type = sys.argv[5]
+        else:
+          show_type = 'calc'
       predicted_data = predict(sys.argv[2], is_save=False)
-      show_predict(predicted_data, model_index, dataset_index)
+      show_predict(predicted_data, model_index, dataset_index, show_type)
     elif sys.argv[1] == 'test':
       test(sys.argv[2])
   except IndexError:
     print('Invalid args number')
     print('data <dataset name> <dataset size> [<net_config>]\t\t\t\tWill be save dataset to ./data/<dataset name>')
     print('learn <dataset name> [<model config name>]\t\t\t\tWill be save model to ./models/<model type>/<date>')
-    print('predict <dataset name> <model config> <dataset name>\t\t\t\tWill be save model to ./models/<model type>/<date>')
+    print('predict <predict config name> [<model index in predict config> <net index in dataset> [<abs or true or pred type of paint>]]\t\t\t\tPredict data from ./models/<dataset>, models from ./models/<model type>/<model>. Names for dataset and model get from configs/predict/<config name>')
 
 if __name__ == '__main__':
   main()
