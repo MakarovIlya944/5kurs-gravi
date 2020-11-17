cd67112c0fb36f9616c07cb42c4b460b5a1efc42
Ilya Makarov <makarovilya944@yandex.ru>
2020-11-17 17:15:33 +0700

    Add predict config, simplify paint

--- gravi/research/paint.py
+++ gravi/research/paint.py
@@ -7,7 +7,7 @@ from .data import DataReader
 
 def show_predict(predicted_data, model_index=None, dataset_index=None, paint_show=None, paint_3d=None):
   data = predicted_data['data']
-  if model_index and dataset_index and paint_show:
+  if not (model_index is None or dataset_index is None or paint_show is None):
     try:
       x,y,c = DataReader.read_folder('data/' + data[model_index]['dataset'])
       true_y = y[dataset_index]
@@ -15,28 +15,18 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
       c = c[dataset_index]
       vm = max(true_y)
 
-      if paint_3d:
-        points, true, calc, pred = [], [], [], []
-        for i in range(c[2]):
-          true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-          pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-          cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
-          calc.append([cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
-          true.append([true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
-          pred.append([pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+      points, true, calc, pred = [], [], [], []
+      for i in range(c[2]):
+        true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+        pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+        cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
+        calc.append([cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+        true.append([true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+        pred.append([pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+      if not paint_3d is None:
         if paint_show == 'mix':
-          pred = np.array(pred)
-          for i,p in np.ndenumerate(pred):
-            if p > 1e-2:
-              pred[i] = int((p/vm)*255)
-            else:
-              pred[i] = int(0)
-          true = np.array(true)
-          for i,p in np.ndenumerate(true):
-            if p > 1e-2:
-              true[i] = int((p/vm)*255)
-            else:
-              true[i] = int(0)
+          pred = normalize(pred)
+          true = normalize(true)
           x, y, z, filled_2, fcolors_2 = strange_magic(pred.astype(np.int32), true.astype(np.int32))
         else:
           if paint_show == 'true':
@@ -47,12 +37,7 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
             points = calc
           else:
             raise Exception(f"Wrong {paint_show} type of show")
-          points = np.array(points)
-          for i,p in np.ndenumerate(points):
-            if p > 1e-2:
-              points[i] = int((p/vm)*255)
-            else:
-              points[i] = int(0)
+          points = normalize(points)
           x, y, z, filled_2, fcolors_2 = strange_magic(points.astype(np.int32))
 
         fig = plt.figure()
@@ -62,18 +47,14 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
         n_plotsx, n_plotsy = 3, 4
         fig, axs = plt.subplots(n_plotsx, n_plotsy)
         for i in range(c[2]):
-          true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-          pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-          cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
-
-          j = i // 4
-          k = i % 4
+          j = i // n_plotsy
+          k = i % n_plotsy
           if paint_show == 'calc':
-            m = [cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+            m = calc[i]
           elif paint_show == 'true':
-            m = [true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+            m = true[i]
           elif paint_show == 'pred':
-            m = [pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+            m = pred[i]
           else:
             raise Exception(f"Wrong {paint_show} type of show")
           axs[j, k].matshow(m,vmax=vm,vmin=0,cmap="Reds")
@@ -88,6 +69,16 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
     labels = [s['name'] for s in data]
     plt.bar(x,y,tick_label=labels)
   plt.show()
+
+def normalize(points, eps=1e-2):
+  points = np.array(points)
+  vm = np.max(points)
+  for i,p in np.ndenumerate(points):
+    if p > eps:
+      points[i] = int((p/vm)*255)
+    else:
+      points[i] = int(0)
+  return points
 
 def strange_magic(pred_p, true_p=None):
   facecolors = pred_p.astype(str)
