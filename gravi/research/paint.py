5bec0efe44811cf34bb2ff48645e6f0e93d7a72f
Ilya Makarov <makarovilya944@yandex.ru>
2020-11-17 13:29:41 +0700

    Add 3d voxels visual

--- gravi/research/paint.py
+++ gravi/research/paint.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d, Axes3D
+import traceback
 import numpy.linalg as l
 from .data import DataReader
 
-
-def show_predict(predicted_data, model_index=None, dataset_index=None, paint_show=None):
+def show_predict(predicted_data, model_index=None, dataset_index=None, paint_show=None, paint_3d=None):
   data = predicted_data['data']
   if model_index and dataset_index and paint_show:
     try:
@@ -13,25 +15,54 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
       c = c[dataset_index]
       vm = max(true_y)
 
-      n_plotsx, n_plotsy = 3, 4
-      fig, axs = plt.subplots(n_plotsx, n_plotsy)
-      for i in range(c[2]):
-        true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-        pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
-        cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
-
-        j = i // 4
-        k = i % 4
-        m = [cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+      if paint_3d:
+        temp, points, true, calc, pred = [], [], [], [], []
+        for i in range(c[2]):
+          true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+          pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+          cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
+          calc.append([cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+          true.append([true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
+          pred.append([pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)])
         if paint_show == 'true':
-          m = [true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+          temp = true
         elif paint_show == 'pred':
-          m = [pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
-        axs[j, k].matshow(m,vmax=vm,vmin=0,cmap="Reds")
-        axs[j, k].set_title(f'z: {-i}')
-      plt.tight_layout()
+          temp = pred
+        elif paint_show == 'calc':
+          temp = calc
+        else:
+          raise Exception("Wrong type of show")
+        for i,ppp in enumerate(temp):
+          for j,pp in enumerate(ppp):
+            for k,p in enumerate(pp):
+              if p > 1e-2:
+                points.append((k, j, i, int((p/vm)*255)))
+        x, y, z, filled_2, fcolors_2 = strange_magic(points, c)
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.voxels(x, y, z, filled_2, facecolors=fcolors_2)
+      else:
+        n_plotsx, n_plotsy = 3, 4
+        fig, axs = plt.subplots(n_plotsx, n_plotsy)
+        for i in range(c[2]):
+          true_z = true_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+          pred_z = pred_y[i*c[0]*c[1]:(i+1)*c[0]*c[1]]
+          cacl_z = [abs(true_z[l] - pred_z[l]) for l in range(len(true_z))]
+
+          j = i // 4
+          k = i % 4
+          m = [cacl_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+          if paint_show == 'true':
+            m = [true_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+          elif paint_show == 'pred':
+            m = [pred_z[k*c[0]:(k+1)*c[0]] for k in range(c[1]-1)]
+          else:
+            raise Exception("Wrong type of show")
+          axs[j, k].matshow(m,vmax=vm,vmin=0,cmap="Reds")
+          axs[j, k].set_title(f'z: {-i}')
+        plt.tight_layout()
     except IndexError as e:
-      print(str(e))
+      traceback.print_exc()
       raise e
   else:
     x = [i for i in range(len(data))]
@@ -39,3 +70,36 @@ def show_predict(predicted_data, model_index=None, dataset_index=None, paint_sho
     labels = [s['name'] for s in data]
     plt.bar(x,y,tick_label=labels)
   plt.show()
+
+def strange_magic(points, c):
+  # build up the numpy logo
+  n_voxels = np.zeros(c, dtype=bool)
+  basecolor = '#FF0000'
+  facecolors = np.where(n_voxels, basecolor + 'FF', '#00000000')
+  for p in points:
+    alpha = str(hex(p[3]))[2:]
+    if p[3] < 16:
+      alpha = '0' + alpha
+    facecolors[p[0]][p[1]][p[2]] = basecolor + alpha
+  filled = np.ones(n_voxels.shape)
+
+  # upscale the above voxel image, leaving gaps
+  filled_2 = explode(filled)
+  fcolors_2 = explode(facecolors)
+
+  # Shrink the gaps
+  x, y, z = np.indices(np.array(filled_2.shape) + 1).astype(float) // 2
+  x[0::2, :, :] += 0.05
+  y[:, 0::2, :] += 0.05
+  z[:, :, 0::2] += 0.05
+  x[1::2, :, :] += 0.95
+  y[:, 1::2, :] += 0.95
+  z[:, :, 1::2] += 0.95
+
+  return x, y, z, filled_2, fcolors_2
+
+def explode(data):
+    size = np.array(data.shape)*2
+    data_e = np.zeros(size - 1, dtype=data.dtype)
+    data_e[::2, ::2, ::2] = data
+    return data_e
--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def main():
       model_index = None
       dataset_index = None
       show_type = None
+      show_3d = None
       if len(sys.argv) > 4:
         model_index = int(sys.argv[3])
         dataset_index = int(sys.argv[4])
@@ -39,15 +40,17 @@ def main():
           show_type = sys.argv[5]
         else:
           show_type = 'calc'
+        if len(sys.argv) > 6:
+          show_3d = sys.argv[6]
       predicted_data = predict(sys.argv[2], is_save=False)
-      show_predict(predicted_data, model_index, dataset_index, show_type)
+      show_predict(predicted_data, model_index, dataset_index, show_type, show_3d)
     elif sys.argv[1] == 'test':
       test(sys.argv[2])
   except IndexError:
     print('Invalid args number')
-    print('data <dataset name> <dataset size> [<net_config>]\t\t\t\tWill be save dataset to ./data/<dataset name>')
-    print('learn <dataset name> [<model config name>]\t\t\t\tWill be save model to ./models/<model type>/<date>')
-    print('predict <predict config name> [<model index in predict config> <net index in dataset> [<abs or true or pred type of paint>]]\t\t\t\tPredict data from ./models/<dataset>, models from ./models/<model type>/<model>. Names for dataset and model get from configs/predict/<config name>')
+    print('data <dataset name> <dataset size> [<net_config>]\t\tWill be save dataset to ./data/<dataset name>')
+    print('learn <dataset name> [<model config name>]\t\tWill be save model to ./models/<model type>/<date>')
+    print('predict <predict config name> [<model index in predict config> <net index in dataset> [<abs or true or pred type of paint>]]\t\tPredict data from ./models/<dataset>, models from ./models/<model type>/<model>. Names for dataset and model get from configs/predict/<config name>')
 
 if __name__ == '__main__':
   main()
